cc81364adb8bd287338471290ed2e560c48b68ef
jakefrasier <ko5865nm@my.lsc.edu>
2017-01-28 21:59:59 -0600

    Add files via upload

---
+++ MonthNamesGUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        enum Month
+        {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            Novemeber,
+            December
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int monthNumber = Convert.ToInt32(textBox1.Text);
+            label2.Text = Convert.ToString((Month)monthNumber);
+        }
+    }
+}
